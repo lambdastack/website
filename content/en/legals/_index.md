1257541c538600ff18d480aef8b4ebdc6d03dfa3
hanscj1 <cjones@cloudm2.com>
2021-12-13 12:56:07 -0500

    Minified hugo

---
+++ content/en/legals/_index.md
@@ -0,0 +1,9 @@
+---
+title: Legal
+type: legal
+robotsdisallow: true
+description: >
+  This is the Legal section for Privacy Policy, Cookies, Terms of Use
+---
+
+<!--add blocks of content here to add more sections to the community page -->
--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@
 # If we want to change to named parameters then we can later.
 
 rm -rf public/
-HUGO_ENV="${3:-production}" hugo --gc || exit 1
+HUGO_ENV="${3:-production}" hugo --gc --minify --cleanDestinationDir || exit 1
 s3deploy -source=public/ -region="${2:-us-east-1}" -bucket="${1:-www.lambdastackio.com}" -acl='public-read'
